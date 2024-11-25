f1d4c6c285d349cf6693161fff9e972fb4177ac2
srprakash168 <prakash.madbhagat@swiftrut.com>
2024-11-26 03:47:58 +0530

    change folder structure on backend folder

--- backend/SocietyHandler/SocietyHandlerRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const societyHandlerController = require("./SocietyHandlerController");
-const asyncHandler = require('express-async-handler');
-
-const societyHandlerRouter = Router()
-
-societyHandlerRouter.post("/create", asyncHandler(societyHandlerController.createChairman))
-
-module.exports = societyHandlerRouter

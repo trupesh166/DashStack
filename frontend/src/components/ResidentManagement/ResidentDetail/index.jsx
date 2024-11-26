3d57ea1b0d21f6e94429e337392e951e852f972b
yash sathwara <yash.sathwara@swiftrut.com>
2024-11-26 21:37:41 +0530

    some changes in 3. Resident Management upload document part

--- frontend/src/components/ResidentManagement/ResidentDetail/index.jsx
+++ frontend/src/components/ResidentManagement/ResidentDetail/index.jsx
@@ -5,8 +5,40 @@ import { DSCard, DSInput, DSSelect } from "../..";
 import Icons from "../../../constants/Icons";
 
 const ResidentDetail = () => {
-  const fileInputRef = useRef(null);
+
+  const aadharFrontRef = useRef(null);
+  const aadharBackRef = useRef(null);
+  const addressProofRef = useRef(null);
+  const rentAgreementRef = useRef(null);
+  const fileInputRef = useRef(null)
+
   const [residentType, setResidentType] = useState("");
+  const [uploadedFiles, setUploadedFiles] = useState({
+    aadharFront: null,
+    aadharBack: null,
+    addressProof: null,
+    rentAgreement: null,
+  });
+
+  const handleFileChange = (event, fileType) => {
+    const file = event.target.files[0];
+    if (file) {
+      setUploadedFiles((prevState) => ({
+        ...prevState,
+        [fileType]: {
+          name: file.name,
+          size: (file.size / (1024 * 1024)).toFixed(2), // File size in MB
+        },
+      }));
+    }
+  };
+
+  const handleFileRemove = (fileType) => {
+    setUploadedFiles((prevState) => ({
+      ...prevState,
+      [fileType]: null,
+    }));
+  };
 
   return (
     <DSCard>
@@ -146,72 +178,167 @@ const ResidentDetail = () => {
       </Row>
 
       <Row gutter={12} className="mb-3">
-
+        {/* Aadhar Front */}
         <Col span={6}>
           <h6>Upload Aadhar Card (Front Side)</h6>
-          <div className={style.document}>
-            <div onClick={() => fileInputRef.current?.click()}>
+          <div
+            className={style.document}
+            onClick={() => aadharFrontRef.current?.click()}
+          >
+            <div>
               <input
                 type="file"
                 className="d-none"
-                ref={fileInputRef}
+                ref={aadharFrontRef}
+                onChange={(e) => handleFileChange(e, "aadharFront")}
               />
               <h4>{Icons.AddImage}</h4>
             </div>
-            <h6 className={style.h6}>Upload a file <span>or drag and drop</span></h6>
-            <p>PNG, JPG, GIF up to 10MB</p>
+            <h6 className={style.h6}>
+              Upload a file <span className={style.span}>or drag and drop</span>
+            </h6>
+            <p className={style.p}>PNG, JPG, GIF up to 10MB</p>
           </div>
+          {uploadedFiles.aadharFront && (
+            <div className={style.uploadDocument}>
+              <div className="d-flex gap-3 align-items-center">
+                <h2>{Icons.Jpg}</h2>
+                <div>
+                  <h6>{uploadedFiles.aadharFront.name}</h6>
+                  <h6 className={style.p}>{uploadedFiles.aadharFront.size} MB</h6>
+                </div>
+              </div>
+              <h2
+                onClick={() => handleFileRemove("aadharFront")}
+                className={style.trashIcon}
+              >
+                {Icons.Trash}
+              </h2>
+            </div>
+          )}
         </Col>
 
+        {/* Aadhar Back */}
         <Col span={6}>
           <h6>Upload Aadhar Card (Back Side)</h6>
-          <div className={style.document}>
-            <div onClick={() => fileInputRef.current?.click()}>
+          <div
+            className={style.document}
+            onClick={() => aadharBackRef.current?.click()}
+          >
+            <div>
               <input
                 type="file"
                 className="d-none"
-                ref={fileInputRef}
+                ref={aadharBackRef}
+                onChange={(e) => handleFileChange(e, "aadharBack")}
               />
               <h4>{Icons.AddImage}</h4>
             </div>
-            <h6 className={style.h6}>Upload a file <span>or drag and drop</span></h6>
-            <p>PNG, JPG, GIF up to 10MB</p>
+            <h6 className={style.h6}>
+              Upload a file <span className={style.span}>or drag and drop</span>
+            </h6>
+            <p className={style.p}>PNG, JPG, GIF up to 10MB</p>
           </div>
+          {uploadedFiles.aadharBack && (
+            <div className={style.uploadDocument}>
+              <div className="d-flex gap-3 align-items-center">
+                <h2>{Icons.Jpg}</h2>
+                <div>
+                  <h6>{uploadedFiles.aadharBack.name}</h6>
+                  <h6 className={style.p}>{uploadedFiles.aadharBack.size} MB</h6>
+                </div>
+              </div>
+              <h2
+                onClick={() => handleFileRemove("aadharBack")}
+                className={style.trashIcon}
+              >
+                {Icons.Trash}
+              </h2>
+            </div>
+          )}
         </Col>
 
+        {/* Address Proof */}
         <Col span={6}>
           <h6>Address Proof (Vera Bill OR Light Bill)</h6>
-          <div className={style.document}>
-            <div onClick={() => fileInputRef.current?.click()}>
+          <div
+            className={style.document}
+            onClick={() => addressProofRef.current?.click()}
+          >
+            <div>
               <input
                 type="file"
                 className="d-none"
-                ref={fileInputRef}
+                ref={addressProofRef}
+                onChange={(e) => handleFileChange(e, "addressProof")}
               />
               <h4>{Icons.AddImage}</h4>
             </div>
-            <h6 className={style.h6}>Upload a file <span>or drag and drop</span></h6>
-            <p>PNG, JPG, GIF up to 10MB</p>
+            <h6 className={style.h6}>
+              Upload a file <span className={style.span}>or drag and drop</span>
+            </h6>
+            <p className={style.p}>PNG, JPG, GIF up to 10MB</p>
           </div>
+          {uploadedFiles.addressProof && (
+            <div className={style.uploadDocument}>
+              <div className="d-flex gap-3 align-items-center">
+                <h2>{Icons.Jpg}</h2>
+                <div>
+                  <h6>{uploadedFiles.addressProof.name}</h6>
+                  <h6 className={style.p}>{uploadedFiles.addressProof.size} MB</h6>
+                </div>
+              </div>
+              <h2
+                onClick={() => handleFileRemove("addressProof")}
+                className={style.trashIcon}
+              >
+                {Icons.Trash}
+              </h2>
+            </div>
+          )}
         </Col>
 
+        {/* Rent Agreement */}
         <Col span={6}>
           <h6>Rent Agreement</h6>
-          <div className={style.document}>
-            <div onClick={() => fileInputRef.current?.click()}>
+          <div
+            className={style.document}
+            onClick={() => rentAgreementRef.current?.click()}
+          >
+            <div>
               <input
                 type="file"
                 className="d-none"
-                ref={fileInputRef}
+                ref={rentAgreementRef}
+                onChange={(e) => handleFileChange(e, "rentAgreement")}
               />
               <h4>{Icons.AddImage}</h4>
             </div>
-            <h6 className={style.h6}>Upload a file <span>or drag and drop</span></h6>
-            <p>PNG, JPG, GIF up to 10MB</p>
+            <h6 className={style.h6}>
+              Upload a file <span className={style.span}>or drag and drop</span>
+            </h6>
+            <p className={style.p}>PNG, JPG, GIF up to 10MB</p>
           </div>
+          {uploadedFiles.rentAgreement && (
+            <div className={style.uploadDocument}>
+              <div className="d-flex gap-3 align-items-center">
+                <h2>{Icons.Jpg}</h2>
+                <div>
+                  <h6>{uploadedFiles.rentAgreement.name}</h6>
+                  <h6 className={style.p}>{uploadedFiles.rentAgreement.size} MB</h6>
+                </div>
+              </div>
+              <h2
+                onClick={() => handleFileRemove("rentAgreement")}
+                className={style.trashIcon}
+              >
+                {Icons.Trash}
+              </h2>
+            </div>
+          )}
         </Col>
-
       </Row>
+
     </DSCard>
   );
 };

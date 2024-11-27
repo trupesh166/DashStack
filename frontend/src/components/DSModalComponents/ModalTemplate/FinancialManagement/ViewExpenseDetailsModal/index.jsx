d70aa1fecb71165c8a24cd5de79a887c5d7b1064
Trupesh Savaliya <trupesh.savaliya@swiftrut.com>
2024-11-28 01:53:13 +0530

    "Updated components and pages in Admin section, including ViewExpenseDetailsModal, CreateComplaint, RequestTracking, and SecurityProtocols, with changes to state management, modal handling, and UI components."

--- frontend/src/components/DSModalComponents/ModalTemplate/FinancialManagement/ViewExpenseDetailsModal/index.jsx
+++ frontend/src/components/DSModalComponents/ModalTemplate/FinancialManagement/ViewExpenseDetailsModal/index.jsx
@@ -9,8 +9,11 @@ export const ViewExpenseDetailsModal = ({
   handleOk,
   handleCancel,
   handleClose,
+  expense,
   IsFooter,
 }) => {
+  if (!expense) return null;
+
   return (
     <div className={styles.viewExpenseDetails}>
       <DSModal
@@ -20,47 +23,46 @@ export const ViewExpenseDetailsModal = ({
         handleOk={handleOk}
         onCancel={handleCancel}
         handleClose={handleClose}
-        IsFooter={IsFooter}
+        IsFooter={false}
         handleContent="Apply"
         disabledButton={false}
       >
         <div>
           <Row className="mb-4">
             <Col span={24}>
-              <h5 className={styles.silver}>Title</h5>
+              <h5 className={style.silver}>Title</h5>
             </Col>
             <Col span={24}>
-              <h5 className={styles.dark}>Rent Or Mortgage</h5>
+              <h5 className={style.dark}>{expense.title}</h5>
             </Col>
           </Row>
 
           <Row className="mb-4">
             <Col span={24}>
-              <h5 className={styles.silver}>Description</h5>
+              <h5 className={style.silver}>Description</h5>
             </Col>
             <Col span={24}>
-              <h5 className={styles.dark}>
-                A visual representation of your spending categories visual
-                representation.{" "}
-              </h5>
+              <h5 className={style.dark}>{expense.description} </h5>
             </Col>
           </Row>
 
           <Row className="mb-4">
             <Col span={12}>
               <Col span={24}>
-                <h5 className={styles.silver}>Date</h5>
+                <h5 className={style.silver}>Date</h5>
               </Col>
               <Col span={24}>
-                <h5 className={styles.dark}>01/02/2024</h5>
+                <h5 className={style.dark}>{expense.date}</h5>
               </Col>
             </Col>
             <Col span={12}>
               <Col span={24}>
-                <h5 className={styles.silver}>Amount</h5>
+                <h5 className={style.silver}>Amount</h5>
               </Col>
               <Col span={24}>
-                <h6 className={styles.dark}>{Icons.Rupee} 1500</h6>
+                <h6 className={style.dark}>
+                  {Icons.Rupee} {expense.amount}
+                </h6>
               </Col>
             </Col>
           </Row>
@@ -70,24 +72,28 @@ export const ViewExpenseDetailsModal = ({
               <h5 className={styles.silver}>Bill</h5>
             </Col>
             <Col span={24}>
-              <Row>
-                <Col span={2}>
-                  <h2>{Icons.Jpg}</h2>
-                </Col>
-                <Col span={20}>
-                  <Row>
-                    <Col span={24}>
-                      <h6 className={styles.dark}>Adharcard Front Side.JPG</h6>
-                    </Col>
-                    <Col span={24}>
-                      <h6 className={styles.silver}>3.5 MB</h6>
-                    </Col>
-                  </Row>
-                </Col>
-                <Col span={2}>
-                  <div className={styles.silver}>{Icons.EyeShow}</div>
-                </Col>
-              </Row>
+              {expense.bill ? (
+                <Row>
+                  <Col span={2}>
+                    <h2>{Icons.Jpg}</h2>
+                  </Col>
+                  <Col span={20}>
+                    <Row>
+                      <Col span={24}>
+                        <h6 className={style.dark}>{expense.bill.name}</h6>
+                      </Col>
+                      <Col span={24}>
+                        <h6 className={style.silver}>{expense.bill.size} MB</h6>
+                      </Col>
+                    </Row>
+                  </Col>
+                  <Col span={2}>
+                    <div className={style.silver}>{Icons.EyeShow}</div>
+                  </Col>
+                </Row>
+              ) : (
+                <h6 className={style.silver}>No bill uploaded</h6>
+              )}
             </Col>
           </Row>
         </div>
